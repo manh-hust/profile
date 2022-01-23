552d2bb08a24be4ec07cc1215efb28da2a8d46a4
ongimkshp <doducmanh.bb@gmail.com>
2022-01-23 22:27:41 +0700

    first commit

---
+++ asscet/js.js
@@ -0,0 +1,15 @@
+
+
+// const btns = document.querySelectorAll('.js-navigation-item');
+// const uu = document.querySelector('.navigation-item')
+//     // console.log (navigations)
+
+
+//     function Show () {
+//         uu.classList.add('active')
+//     }
+
+//     for (const btn of btns) {
+//         btn.addEventListener('click', Show)
+//     }
+
