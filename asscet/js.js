10d652309e01f0d6df093854332630af1cb8ebdd
ongimkshp <doducmanh.bb@gmail.com>
2022-01-26 22:06:00 +0700

    ok-j-query

--- asscet/js.js
+++ asscet/js.js
@@ -1,15 +1,55 @@
+const self = $('#self-id')
+const education = $('#Education-id')
+const target = $('#target-id')
+const interest = $('#interest-id')
 
+const btnSelf = $('.menu-self')
+const btnEducation = $('.menu-education')
+const btnTarget = $('.menu-target')
+const btnInterest = $('.menu-interest')
 
-// const btns = document.querySelectorAll('.js-navigation-item');
-// const uu = document.querySelector('.navigation-item')
-//     // console.log (navigations)
+education.hide()
+target.hide()
+interest.hide()
 
-
-//     function Show () {
-//         uu.classList.add('active')
-//     }
-
-//     for (const btn of btns) {
-//         btn.addEventListener('click', Show)
-//     }
+btnSelf.on('click', function() {
+    education.hide()
+    target.hide()
+    interest.hide()
+    self.show()
+    btnSelf.addClass('active')
+    btnEducation.removeClass('active')
+    btnTarget.removeClass('active')
+    btnInterest.removeClass('active')
+})
+btnEducation.on('click', function() {
+    education.show()
+    target.hide()
+    interest.hide()
+    self.hide()
+    btnSelf.removeClass('active')
+    btnEducation.addClass('active')
+    btnTarget.removeClass('active')
+    btnInterest.removeClass('active')
+})
+btnTarget.on('click', function() {
+    education.hide()
+    target.show()
+    interest.hide()
+    self.hide()
+    btnSelf.removeClass('active')
+    btnEducation.removeClass('active')
+    btnTarget.addClass('active')
+    btnInterest.removeClass('active')
+})
+btnInterest.on('click', function() {
+    education.hide()
+    target.hide()
+    interest.show()
+    self.hide()
+    btnSelf.removeClass('active')
+    btnEducation.removeClass('active')
+    btnTarget.removeClass('active')
+    btnInterest.addClass('active')
+})
 
